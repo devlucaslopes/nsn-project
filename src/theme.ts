ec3c11d9c1f99ea6dc9da4801a3150387ceb7900
Lucas Lopes <dev.thuram@gmail.com>
2022-04-27 09:16:19 -0300

    create theme config file

---
+++ src/theme.ts
@@ -0,0 +1,43 @@
+export const theme = {
+  container: '141.6rem',
+  colors: {
+    primary: {
+      100: '#CFF0E2',
+      200: '#74CFA7',
+      300: '#1EA86C',
+      400: '#214334',
+      500: '#010101'
+    },
+    gray: {
+      100: '#D0D0D0',
+      200: '#737373',
+      300: '#161616',
+      400: '#0B0B0B'
+    },
+    black: '#000000',
+    white: '#FFFFFF'
+  },
+  fonts: {
+    family: {
+      headings: 'Share Tech Mono',
+      body: 'Roboto'
+    },
+    sizes: {
+      normal: '1.6rem',
+      medium: '1.8rem',
+      large: '2.4rem',
+      h1: '9.6rem',
+      h2: '6.4rem',
+      h3: '4.8rem',
+      h4: '3.2rem'
+    }
+  },
+  spacings: {
+    normal: '1.6rem',
+    medium: '1.8rem',
+    large: '2.4rem',
+    xlarge: '3.2rem',
+    xxlarge: '4.8rem',
+    huge: '6.4rem'
+  }
+}
