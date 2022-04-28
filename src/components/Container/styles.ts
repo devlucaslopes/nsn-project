23a0140d3cdc6fd298e60ada843eb25389d32bd8
Lucas Lopes <dev.thuram@gmail.com>
2022-04-28 08:38:24 -0300

    remove padding when is large

--- src/components/Container/styles.ts
+++ src/components/Container/styles.ts
@@ -1,10 +1,14 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 export const Wrapper = styled.div`
   ${({ theme }) => css`
     max-width: ${theme.container};
     margin: 0 auto;
     padding: 0 ${theme.spacings.normal};
-    background: #ccc;
+
+    ${media.greaterThan('large')`
+      padding: 0;
+    `}
   `}
 `
