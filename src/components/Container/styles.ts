86f0a10f528775b1a6308a7e7a48ba4ce93f3e3a
Lucas Lopes <dev.thuram@gmail.com>
2022-04-29 09:01:08 -0300

    added position relative to container

--- src/components/Container/styles.ts
+++ src/components/Container/styles.ts
@@ -3,6 +3,7 @@ import media from 'styled-media-query'
 
 export const Wrapper = styled.div`
   ${({ theme }) => css`
+    position: relative;
     max-width: ${theme.container};
     margin: 0 auto;
     padding: 0 ${theme.spacings.normal};
