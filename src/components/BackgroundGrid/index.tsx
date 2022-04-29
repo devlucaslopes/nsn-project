9ec85350fd21d6a98ae23a40a93ccbaba3aff06b
Lucas Lopes <dev.thuram@gmail.com>
2022-04-29 09:00:44 -0300

    create background grid component

---
+++ src/components/BackgroundGrid/index.tsx
@@ -0,0 +1,63 @@
+import { Wrapper } from './styles'
+
+export const BackgroundGrid = () => {
+  return (
+    <Wrapper>
+      <svg
+        viewBox="0 0 1920 1021"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+      >
+        <mask
+          id="mask0_34_55"
+          maskUnits="userSpaceOnUse"
+          x="0"
+          y="0"
+          width="1920"
+          height="1021"
+        >
+          <rect width="1920" height="1021" fill="url(#pattern0)" />
+        </mask>
+        <g mask="url(#mask0_34_55)">
+          <rect
+            width="1920"
+            height="1021"
+            fill="url(#paint0_linear_34_55)"
+            fillOpacity="0.3"
+          />
+        </g>
+        <defs>
+          <pattern
+            id="pattern0"
+            patternContentUnits="objectBoundingBox"
+            width="0.0442708"
+            height="0.0832517"
+          >
+            <use
+              xlinkHref="#image0_34_55"
+              transform="scale(0.000442708 0.000832517)"
+            />
+          </pattern>
+          <linearGradient
+            id="paint0_linear_34_55"
+            x1="2.99996"
+            y1="4.67261e-05"
+            x2="1212.56"
+            y2="760.209"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#151515" />
+            <stop offset="1" stopColor="#4D4D4D" stopOpacity="0" />
+          </linearGradient>
+          <image
+            id="image0_34_55"
+            width="100"
+            height="100"
+            xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAYAAABw4pVUAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAADySURBVHgB7dFJAQAgEACh1f6dPVrMAyqwzjN0COn4F3tIERIjJEZIjJAYITFCYoTECIkREiMkRkiMkBghMUJihMQIiRESIyRGSIyQGCExQmKExAiJERIjJEZIjJAYITFCYoTECIkREiMkRkiMkBghMUJihMQIiRESIyRGSIyQGCExQmKExAiJERIjJEZIjJAYITFCYoTECIkREiMkRkiMkBghMUJihMQIiRESIyRGSIyQGCExQmKExAiJERIjJEZIjJAYITFCYoTECIkREiMkRkiMkBghMUJihMQIiRESIyRGSIyQGCExQmKExAiJWecZMi7OaRC150igYwAAAABJRU5ErkJggg=="
+          />
+        </defs>
+      </svg>
+    </Wrapper>
+  )
+}
---
+++ src/components/BackgroundGrid/styles.ts
@@ -0,0 +1,13 @@
+import styled, { css } from 'styled-components'
+
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    top: 0;
+    width: 300%;
+    height: 100vh;
+    position: absolute;
+
+    > svg {
+    }
+  `}
+`
